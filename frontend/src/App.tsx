f386712395e4ad7be4e8108f0d530559f718e878
Tejas Chaudhari <jaybalaji192@gmail.com>
2025-01-27 09:21:15 +0530

    ..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cookies from 'js-cookie'
 import { Header } from './components/Header';
 import { BookDetails } from './pages/BookDetails';
